3105d285e10e5eb0cb5381557ac1f470892ae519
David <daviduwu123@gmail.com>
2026-04-20 15:02:27 -0600

    build: setup vercel adapter for ssr and update astro deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,21 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: vercel(),
 
   server: {
     port: 4321,
   },
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      // @ts-expect-error - Ignorando disparidad interna de tipos de Vite entre Astro y Tailwind
+      tailwindcss()
+    ]
   }
 });
